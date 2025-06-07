a08515226f1ea868fc8ddea94abf764aa3313562
CodewithMK180105 <manishkumar180105@gmail.com>
2025-06-07 09:48:47 +0530

    Created userRoutes, and imported in the server.js

---
+++ server/routes/user.routes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { checkAuth, login, signUp, updateProfile } from '../controllers/user.controllers';
+import { protectedRoute } from '../middleware/auth';
+
+const userRouter = express.Router();
+
+userRouter.post('/signup', signUp);
+userRouter.post('/login', login);
+userRouter.put('/update-profile', protectedRoute, updateProfile);
+userRouter.get('/check', protectedRoute, checkAuth);
+
+export default userRouter;
+// This code defines the user routes for signup, login, profile update, and authentication check.
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/user.routes.js";
 
 // Create Express app and HTTP server
 const app=express();
@@ -12,6 +13,7 @@ const server=http.createServer(app); // we are using the http server because we 
 app.use(express.json({limit: "4mb"})); // Limit the request body size to 4mb, express.json() because all the request to be passed to server should be in json format
 app.use(cors()); // Enable CORS for all routes
 app.use("/api/status", (req, res)=> res.send("Server is running")); // Simple status endpoint
+app.use("/api/auth", userRouter); // User authentication routes
 
 // Connect to the database
 await connectDB(); // Connect to the MongoDB database
