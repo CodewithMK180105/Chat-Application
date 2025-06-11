7ff50e50636b1ac1e4f425a6e69a5e994305d14f
CodewithMK180105 <manishkumar180105@gmail.com>
2025-06-11 17:23:54 +0530

    written auth context to provide functionalities.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import HomePage from './pages/HomePage'
 import ProfilePage from './pages/ProfilePage'
 import Login from './pages/LoginPage'
 import { Routes, Route } from 'react-router-dom'
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Toaster />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<Login />} />
